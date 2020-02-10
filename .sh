9f46bc15fddfd6e49bd1b1796a4b5cce8996db14
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-02-10 17:34:47 +0100

    Update .sh

--- .sh
+++ .sh
@@ -2,4 +2,8 @@ if [ -x /usr/bin/apt ]; then
 echo APT
 elif [ -x /usr/bin/yum ]; then
 echo YUM
+sudo yum install bash-completion
+sudo yum install PackageKit-command-not-found
+sudo yum install podman-docker
+sudo yum install nano
 fi
